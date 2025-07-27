7c9c9cf2b0c9c8f92aa8e04d364ce9fdf8ea3d65
erictita <nkenatita@gmail.com>
2025-07-27 09:00:46 +0200

    added comment to test pull request

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
  
 # EC2 instances
 resource "aws_instance" "example" {
-  count         = 2
+  count         = 5
   ami           = "ami-0a0409af1cb831414"
   instance_type = "t2.micro"
  
